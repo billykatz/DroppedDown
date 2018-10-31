48a35abc019fc2a670f6baadeb2c0eb1a449f2f0
Katz, Billy-CW <Billy.Katz@bestbuy.com>
2018-10-31 12:55:04 -0700

    First test created

--- DownFallTests/DownFallTests.swift
+++ DownFallTests/DownFallTests.swift
@@ -21,9 +21,33 @@ class DownFallTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func compareTuples <T:Equatable> (_ first:[(T,T)],_ second:[(T,T)]) -> Bool
+    {
+        guard first.count == second.count else { return false }
+        var verifiedTuples = 0
+        for tuple in first {
+            for innerTuple in second {
+                if tuple.0 == innerTuple.0 && tuple.1 == innerTuple.1 {
+                    verifiedTuples += 1
+                }
+            }
+        }
+        return verifiedTuples == first.count
+    }
+    
+    func testFindNeighbors () {
+        let tiles =
+            [[DFTileSpriteNode.init(type: .rock(RockData.init(textureName: "player2")), search: .white),
+              DFTileSpriteNode.init(type: .rock(RockData.init(textureName: "exit")), search: .white),
+              DFTileSpriteNode.init(type: .rock(RockData.init(textureName: "blueRock")), search: .white)],
+             [DFTileSpriteNode.init(type: .rock(RockData.init(textureName: "blueRock")), search: .white), DFTileSpriteNode.init(type: .rock(RockData.init(textureName: "blueRock")), search: .white), DFTileSpriteNode.init(type: .rock(RockData.init(textureName: "blueRock")), search: .white)],
+             [DFTileSpriteNode.init(type: .rock(RockData.init(textureName: "blueRock")), search: .white), DFTileSpriteNode.init(type: .rock(RockData.init(textureName: "blueRock")), search: .white), DFTileSpriteNode.init(type: .rock(RockData.init(textureName: "blueRock")), search: .white)]]
+        
+        let board = Board.init(tiles, size: 3, playerPosition: (0,0), exitPosition: (0,1), buttons: [])
+        
+        let neighbors = board.findNeighbors(1,1)
+        XCTAssert(compareTuples(neighbors, [(0,2), (1,0), (1,1), (1, 2), (2,0), (2,1) ,(2,2)]), "Neighbors found")
+        
     }
     
     func testPerformanceExample() {

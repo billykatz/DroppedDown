b575103d6f6d4429c0087500ef6267b89bbf162d
William Katz <wfk213@nyu.edu>
2019-01-08 20:47:11 -0800

    Added Referee and tests

---
+++ DownFall/Models/Difficulty.swift
@@ -0,0 +1,9 @@
+//
+//  Difficulty.swift
+//  DownFall
+//
+//  Created by William Katz on 12/24/18.
+//  Copyright © 2018 William Katz LLC. All rights reserved.
+//
+
+import Foundation
---
+++ DownFallTests/GameRefereeTests.swift
@@ -0,0 +1,9 @@
+//
+//  GameRefereeTests.swift
+//  DownFallTests
+//
+//  Created by William Katz on 12/29/18.
+//  Copyright © 2018 William Katz LLC. All rights reserved.
+//
+
+import Foundation

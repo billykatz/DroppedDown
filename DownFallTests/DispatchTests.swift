d27d0010b9d2af7330c6069e2c9e9608bf1ec71c
William Katz <wfk213@nyu.edu>
2019-03-09 13:45:19 -0800

    Added files to xcode project Added tests for Renderer and Dispatch

--- DownFallTests/DispatchTests.swift
+++ DownFallTests/DispatchTests.swift
@@ -7,27 +7,163 @@
 //
 
 import XCTest
+@testable import DownFall
 
-class Dispatch: XCTestCase {
+class MockRenderer : DispatchReceiver {
+    
+    var receivedWasCalled = false
+    var eventReceived : [Event: Bool] = [:]
+    
+    func receive(_ e: Event) {
+        //purposefully left empty for protocol conformance
+        eventReceived[e] = true
+    }
+    func isEqual(to other: AnyObject) -> Bool {
+        guard let other = other as? MockRenderer else { return false }
+        return self === other
+        
+    }
+    
+}
 
+class MockBoard : DispatchReceiver {
+    var eventReceived : [Event: Bool] = [:]
+    
+    func receive(_ e: Event) {
+        //purposefully left empty for protocol conformance
+        eventReceived[e] = true
+    }
+    func isEqual(to other: AnyObject) -> Bool {
+        guard let other = other as? MockBoard else { return false }
+        return self === other
+        
+    }
+    
+}
+
+class DispatchTests: XCTestCase {
+    
+    var dispatch: Dispatch = Dispatch.sharedInstance
+    var mockRenderer: MockRenderer!
+    var mockBoard: MockBoard!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        mockRenderer = MockRenderer()
+        mockBoard = MockBoard()
     }
-
+    
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mockRenderer = nil
+        mockBoard = nil
+        dispatch.reset()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testDispatchInit() {
+        let dispatch = Dispatch()
+        XCTAssertNotNil(dispatch)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testDispatchProvidesSingleton() {
+        XCTAssertNotNil(dispatch)
     }
-
+    
+    func testDispatchRegistration() {
+        
+        //registration
+        XCTAssertEqual(dispatch.receivers[.render]?.count, nil)
+        XCTAssertEqual(dispatch.receivers[.rotate]?.count, nil)
+        
+        //register for render
+        dispatch.register(mockRenderer, for: .render)
+        XCTAssertEqual(dispatch.receivers[.render]?.count, 1)
+        dispatch.register(mockBoard, for: .render)
+        XCTAssertEqual(dispatch.receivers[.render]?.count, 2)
+        
+        //register for rotate
+        dispatch.register(mockBoard, for: .rotate)
+        XCTAssertEqual(dispatch.receivers[.rotate]?.count, 1)
+        dispatch.register(mockRenderer, for: .rotate)
+        XCTAssertEqual(dispatch.receivers[.rotate]?.count, 2)
+        
+        
+        //Unregister
+        
+        //unregister for rotate
+        dispatch.unregister(mockRenderer, for: .rotate)
+        XCTAssertEqual(dispatch.receivers[.rotate]?.count, 1)
+        dispatch.unregister(mockBoard, for: .rotate)
+        XCTAssertEqual(dispatch.receivers[.rotate]?.count, 0)
+        
+        //unregister for render
+        dispatch.unregister(mockRenderer, for: .render)
+        XCTAssertEqual(dispatch.receivers[.render]?.count, 1)
+        dispatch.unregister(mockBoard, for: .render)
+        XCTAssertEqual(dispatch.receivers[.render]?.count, 0)
+        
+        
+        //registration
+        dispatch.register(mockRenderer, for: .render)
+        dispatch.register(mockBoard, for: .rotate)
+        
+        XCTAssertEqual(dispatch.receivers[.render]?.count, 1)
+        XCTAssertEqual(dispatch.receivers[.rotate]?.count, 1)
+        
+        //unregistrations
+        //ensure we dont unregister receivers from stuff they arent listening too
+        dispatch.unregister(mockBoard, for: .render)
+        XCTAssertEqual(dispatch.receivers[.render]?.count, 1)
+        
+        dispatch.unregister(mockRenderer, for: .rotate)
+        XCTAssertEqual(dispatch.receivers[.rotate]?.count, 1)
+    }
+    
+    func testDispatchSendsEvent() {
+        dispatch.register(mockRenderer, for: .render)
+        dispatch.register(mockBoard, for: .rotate)
+        
+        XCTAssertFalse(mockBoard.eventReceived[.rotate] ?? false)
+        XCTAssertFalse(mockRenderer.eventReceived[.render] ?? false)
+        
+        dispatch.send()
+        
+        XCTAssertFalse(mockBoard.eventReceived[.rotate] ?? false)
+        XCTAssertFalse(mockRenderer.eventReceived[.render] ?? false)
+        
+        
+        dispatch.post(.rotate)
+        dispatch.post(.render)
+        
+        dispatch.send()
+        XCTAssertTrue(mockBoard.eventReceived[.rotate] ?? false)
+        XCTAssertFalse(mockRenderer.eventReceived[.render] ?? false)
+        XCTAssertFalse(mockBoard.eventReceived[.render] ?? false)
+        XCTAssertFalse(mockRenderer.eventReceived[.rotate] ?? false)
+        
+        dispatch.send()
+        XCTAssertTrue(mockBoard.eventReceived[.rotate] ?? false)
+        XCTAssertTrue(mockRenderer.eventReceived[.render] ?? false)
+        XCTAssertFalse(mockBoard.eventReceived[.render] ?? false)
+        XCTAssertFalse(mockRenderer.eventReceived[.rotate] ?? false)
+        
+        
+        dispatch.register(mockBoard, for: .render)
+        XCTAssertFalse(mockBoard.eventReceived[.render] ?? false)
+        dispatch.post(.render)
+        dispatch.send()
+        XCTAssertTrue(mockBoard.eventReceived[.rotate] ?? false)
+        XCTAssertTrue(mockRenderer.eventReceived[.render] ?? false)
+        XCTAssertTrue(mockBoard.eventReceived[.render] ?? false)
+        XCTAssertFalse(mockRenderer.eventReceived[.rotate] ?? false)
+        
+        
+        dispatch.register(mockRenderer, for: .rotate)
+        XCTAssertFalse(mockRenderer.eventReceived[.rotate] ?? false)
+        dispatch.post(.rotate)
+        dispatch.send()
+        XCTAssertTrue(mockBoard.eventReceived[.rotate] ?? false)
+        XCTAssertTrue(mockRenderer.eventReceived[.render] ?? false)
+        XCTAssertTrue(mockBoard.eventReceived[.render] ?? false)
+        XCTAssertTrue(mockRenderer.eventReceived[.rotate] ?? false)
+        
+    }
 }
--- DownFallTests/RendererTests.swift
+++ DownFallTests/RendererTests.swift
@@ -11,9 +11,29 @@ import SpriteKit
 @testable import DownFall
 
 class RendererTests: XCTestCase {
-
-//    func testAddSprites() {
-//        let renderer = Renderer(playableRect: CGRect.zero, foreground: SKNode(), board: , size: <#T##Int#>)
-//    }
+    let boardSize = 10
+    let mockTileCount = 10 * 10
+    var board : Board!
+    var greenBoard : Builder!
+    var renderer: Renderer!
+    
+    
+    override func setUp() {
+        board = Board.build(size: boardSize)
+        greenBoard = all(.greenRock, board)
+        renderer = Renderer(playableRect: CGRect.zero,
+                                foreground: SKNode(),
+                                board: greenBoard(board))
+    }
+    
+    func testAddSprites() {
+        //TODO: Add some functionality on Renderer that allows us to know how many children it has, including headers and controls etc...
+        XCTAssertTrue(mockTileCount < renderer.foreground.children.count, "The foreground should contain at least the 100 sprites we added. It may contain more because of things like the header and controls that are also added as children")
+    }
+    
+    func testCreateSprites() {
+        let count = renderer.createSprites(from: greenBoard(board)).flatMap { $0 }.count
+        XCTAssertEqual(mockTileCount, count)
+    }
 
 }

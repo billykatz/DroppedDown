7c1ab85f87fc4a298fcb8de1bfcdc3c4cd47ebb0
William Katz <wfk213@nyu.edu>
2019-03-10 16:35:00 -0700

    Moving around some project files adding assets adding menu sprite node

---
+++ DownFall/Views/MenuSpriteNode.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  DownFall
+//
+//  Created by William Katz on 3/10/19.
+//  Copyright © 2019 William Katz LLC. All rights reserved.
+//
+
+import Foundation
+
+class Menu: SKSpriteNode {
+    
+    convenience init(playableRect: CGRect) {
+
+    }
+}

ccf03d7d438219f0883d127b0d4dbe3cb02383f8
William Katz <wfk213@nyu.edu>
2019-04-02 17:50:05 -0700

    Fixing tests

--- DownFallTests/TestHelpers.swift
+++ DownFallTests/TestHelpers.swift
@@ -11,38 +11,38 @@ import Foundation
 
 extension TileType {
     static var mouthyMonster: TileType {
-        return TileType.greenMonster(CombatTileData(hp: 1, attacksThisTurn: 0, weapon: .mouth))
+        return TileType.greenMonster(CombatTileData(hp: 1, attacksThisTurn: 0, weapon: .mouth, hasGem: false))
     }
     
     static var pickAxeMonster: TileType {
-        return TileType.greenMonster(CombatTileData(hp: 1, attacksThisTurn: 0, weapon: .pickAxe))
+        return TileType.greenMonster(CombatTileData(hp: 1, attacksThisTurn: 0, weapon: .pickAxe, hasGem: false))
     }
     
     static var strongPlayer: TileType {
-        return TileType.player(CombatTileData(hp: 3, attacksThisTurn: 0, weapon: .strongPickAxe))
+        return TileType.player(CombatTileData(hp: 3, attacksThisTurn: 0, weapon: .strongPickAxe, hasGem: false))
     }
     
     static var deadPlayer: TileType {
-        return TileType.player(CombatTileData(hp: 0, attacksThisTurn: 0, weapon: .strongPickAxe))
+        return TileType.player(CombatTileData(hp: 0, attacksThisTurn: 0, weapon: .strongPickAxe, hasGem: false))
     }
     
     static var strongMonster: TileType {
-        return TileType.player(CombatTileData(hp: 3, attacksThisTurn: 0, weapon: .mouth))
+        return TileType.player(CombatTileData(hp: 3, attacksThisTurn: 0, weapon: .mouth, hasGem: false))
     }
     
     static var normalMonster: TileType {
-        return TileType.player(CombatTileData(hp: 2, attacksThisTurn: 0, weapon: .mouth))
+        return TileType.player(CombatTileData(hp: 2, attacksThisTurn: 0, weapon: .mouth, hasGem: false))
     }
     
     static var monsterThatHasAttacked: TileType {
-        return TileType.greenMonster(CombatTileData(hp: 1, attacksThisTurn: 1, weapon: .mouth))
+        return TileType.greenMonster(CombatTileData(hp: 1, attacksThisTurn: 1, weapon: .mouth, hasGem: false))
     }
 
 }
 
 extension CombatTileData {
     func subtractHP(_ amount: Int) -> CombatTileData {
-        return CombatTileData(hp: self.hp - amount, attacksThisTurn: self.attacksThisTurn, weapon: self.weapon)
+        return CombatTileData(hp: self.hp - amount, attacksThisTurn: self.attacksThisTurn, weapon: self.weapon, hasGem: self.hasGem)
     }
 }
 
